4494057037331d07da87f0af9aa7ef02305defad
TAINA VIEIRA DE ARAUJO <00001107574183sp@aluno.educacao.sp.gov.br>
2024-07-30 10:46:18 -0300

    nome e senha de multiplos usuarios

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
+const usuarios = [
+    {
+        usuario: 'taina',
+        senha: '1234'
+    },
+    {
+        usuario: 'Suzany',
+        senha: '6670'
+    },
+    {
+        usuario: 'admin',
+        senha: '0987'
+    },
+    {
+        usuario: 'miku',
+        senha: '1010'
+    }
+]
+
 document.getElementById('formulario').addEventListener('submit',function login(event){
     event.preventDefault();
 
     var user = document.getElementById('user').value;
     var senha = document.getElementById('senha').value;
+    var loginValido = false;
 
-    if (user === "taina" && senha === "1234"){
-        alert('Sucesso');
+    for (var i in usuarios){
+        if (user == usuarios[i].usuario && senha == usuarios[i].senha){
+            loginValido = true;
+            break;
+    }
+}
+
+    if(loginValido == true){
+        alert('sucesso');
         location.href = "home.html";
     }else{
-        alert('Usuário ou senha incorreto');
-    }
-});
+        alert('usuario ou senha incorreta');
+    }});
