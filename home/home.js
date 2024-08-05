d365412427509e81443335227f67744c017bd9a8
TAINA VIEIRA DE ARAUJO <00001107574183sp@aluno.educacao.sp.gov.br>
2024-08-05 13:19:12 -0300

    sistema de ppontuação funcionando

--- home/home.js
+++ home/home.js
@@ -3,5 +3,5 @@ let pontos = 0;
 document.getElementById('pontos').addEventListener('click',function (){
 
 pontos += 10;
-document.getElementById('pontuacao').textContent = 'Pontuação: '
+document.getElementById('pontuacao').textContent = 'Pontuação: ' + pontos;
 })
